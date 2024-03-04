5b2e65c65146b424453d46e7b4de65b9e91ae535
allwinn <allwinnoble@gmail.com>
2024-03-04 11:26:45 +0100

    adding the build directory

---
+++ build/CMakeFiles/challenge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/challenge.dir/asaphus_coding_challenge.cpp.o"
+  "challenge"
+  "challenge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/challenge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
